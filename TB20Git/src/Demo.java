918db87ed79cfb225503c8881ce46608dc6c3a18
Vaishanavi887 <110394513+Vaishanavi887@users.noreply.github.com>
2022-08-02 17:10:43 +0530

    by git2

--- TB20Git/src/Demo.java
+++ TB20Git/src/Demo.java
@@ -4,6 +4,8 @@ public static void main(String[] args) {
 	System.out.println("Git 20");
 		System.out.println("Git 22");
 			System.out.println("Git 23");
+				System.out.println("Git 24");
+
 
 
 }
